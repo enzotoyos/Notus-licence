6744538c336ceda50e464a36809202546cc2abd6
enzotoyos <enzo.toyos@gmail.com>
2023-04-11 14:22:24 +0200

    first commit

---
+++ src/views/auth/ForgotPassword.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ForgotPassword() {
+  return (
+    <>
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-4/12 px-4">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+              <div className="rounded-t mb-0 px-6 py-6">
+                <div className="text-center mb-3">
+                  <h6 className="text-blueGray-500 text-sm font-bold">
+                    Mot de passe oublié
+                  </h6>
+                </div>
+              </div>
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <form>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Email"
+                    />
+                  </div>
+
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      valider
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="flex flex-wrap mt-6 relative">
+              <div className="w-1/2">
+                <Link to="/auth/login" className="text-blueGray-200">
+                  <small>J'ai déja un compte?</small>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
